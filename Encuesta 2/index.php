b851879ad6a4a79a1e667f58ed22b53d5a6e5346
mariorivzz <mariomariorivasrivas@gmail.com>
2021-12-10 20:22:42 +0100

    Añadiento doas las cosas

---
+++ Encuesta 2/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$emocional1 = $_POST['emocionalmente1'];
+$emocional2 = $_POST['emocionalmente2'];
+$emocional3 = $_POST['emocionalmente3'];
+
+echo "$emocionalmente1 " . " <br> $emocionalmente2 <br>" . $emocionalmente3;
+
+?>
